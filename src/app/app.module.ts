ea1f3a50bcb470bad4aa7c22b6f4ddf35f75240a
ryma1107 <jebrirym91@gmail.com>
2020-08-28 13:35:33 +0100

    Checkpoint4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PereComponent } from './pere/pere.component';
+import { FilsComponent } from './fils/fils.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PereComponent,
+    FilsComponent
   ],
   imports: [
     BrowserModule,
